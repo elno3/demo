7db0b5d9f88f04d7fc2699628368efb4146637ea
SantirA <SantirA@DESKTOP-DN0V1E3>
2020-04-07 16:04:47 +0200

    combination with list

--- src/test/java/com/example/demo/Combination.java
+++ src/test/java/com/example/demo/Combination.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.basra.Card;
@@ -12,14 +13,14 @@ class Combination {
 	start & end ---> Staring and Ending indexes in arr[] 
 	index ---> Current index in data[] 
 	r ---> Size of a combination to be printed */
-	static void combinationUtil(Card arr[], Card data[], int start, 
+	static void combinationUtil(List<Card> listCards, List<Card> outputCombination, int start, 
 								int end, int index, int r) 
 	{ 
 		// Current combination is ready to be printed, print it 
 		if (index == r) 
 		{ 
 			for (int j=0; j<r; j++) 
-				System.out.print(data[j]+" "); 
+				System.out.print(outputCombination.get(j)+" "); 
 			System.out.println(""); 
 			return; 
 		} 
@@ -30,20 +31,20 @@ class Combination {
 		// at remaining positions 
 		for (int i=start; i<=end && end-i+1 >= r-index; i++) 
 		{ 
-			data[index] = arr[i]; 
-			combinationUtil(arr, data, i+1, end, index+1, r); 
+			outputCombination.add(index, listCards.get(i)); 
+			combinationUtil(listCards, outputCombination, i+1, end, index+1, r); 
 		} 
 	} 
 
 	// The main function that prints all combinations of size r 
 	// in arr[] of size n. This function mainly uses combinationUtil() 
-	static void printCombination(Card arr[], int n, int r) 
+	static void printCombination(List<Card> cardList, int n, int r) 
 	{ 
 		// A temporary array to store all combination one by one 
-		Card data[]=new Card[r]; 
+		List<Card> lisCombination=new ArrayList<Card>(); 
 
 		// Print all combination using temprary array 'data[]' 
-		combinationUtil(arr, data, 0, n-1, 0, r); 
+		combinationUtil(cardList, lisCombination, 0, n-1, 0, r); 
 	} 
 
 	/*Driver function to check for above function*/
@@ -51,13 +52,12 @@ class Combination {
 		
 		List<Card> card = List.of(new Card(Types.HEART, "one"), new Card(Types.CLUB, "one"),new Card(Types.HEART, "two"),new Card(Types.HEART, "ten"));
 		
-		Card[] arr = card.toArray(new Card [] {});
 		 
-		int n = arr.length; 
+		int n = card.size(); 
 		
 		for(int i =1;i<=n;i++) {
 			
-			printCombination(arr, n, i); 
+			printCombination(card, n, i); 
 		}
 	} 
 } 
